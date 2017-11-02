28c638baa26e2d51816eb36b08c5dc42a2256e13
suhanlee <suhan.lee.k@gmail.com>
2017-11-02 11:50:10 +0900

    modify 1_1.cpp

--- cpp/exam1/1_1.cpp
+++ cpp/exam1/1_1.cpp
@@ -3,14 +3,28 @@
 
 using namespace std;
 
+// with hash-map tc = O(n), sc = O(1)
 bool check_string_unique(string str) {
     map<char, bool> m;
 
     for (auto elem : str) {
-        if (m[elem] == false) {
-            m[elem] = true;
-        } else {
+        if (m[elem]) {
             return false;
+        }
+
+        m[elem] = true;
+    }
+
+    return true;
+}
+
+// without data structure O(n^2)
+bool check_string_unique_without_ds(string str) {
+    for(int i = 0; i < str.length(); i++) {
+        for(int j = 0; j < str.length(); j++) {
+            if (i != j && str[i] == str[j]) {
+                return false;
+            }
         }
     }
 
@@ -20,4 +34,6 @@ bool check_string_unique(string str) {
 int main() {
     cout << check_string_unique("abcd") << endl;
     cout << check_string_unique("abb") << endl;
+    cout << check_string_unique_without_ds("abc") << endl;
+    cout << check_string_unique_without_ds("abcc") << endl;
 }
